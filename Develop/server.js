0ccc766c85d9fb4eec343da285ecee17f69785be
Calvin Swomley <calvinswomley@gmail.com>
2021-11-09 12:34:23 -0700

    GET routes setup

--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,40 @@
 const express = require("express");
-const app = express();
+
 //Need absolute path so define it here. Note that 'path' comes with NodeJS
 const path = require("path");
 
+const db = require("./db/db.json");
 const PORT = 3003;
+const app = express();
+
+
+
+//allow express to parse data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//set up of static middleware >> This will grab all the static files in the 'public' folder
+app.use(express.static("public"))
 
 //define routes (underscore means 'req' is being taken in but not used)
 
 //1. post route
-app.post('/api/notes', (__, res)=> {
+app.post('/api/notes', (req, res)=> {
     res.send("Hello World")
 })
+
 //2. get route (/api/notes - read and return all notes from db.json)
-app.get('/api/notes', (__, res) => {
-    res.send(path.join(__dirname, "./db.json"))
-})
+app.get('/api/notes', (__, res) => res.json(db));
+
 //3. get route (/notes - return notes.html file)
-app.get('/notes', (__, res) => {
-    res.send(path.join(__dirname, "../public/notes.html"))
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
+
 //4. get route (* - return index.html file)
-app.get('*', (__, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-})
+app.get('/', (req, res) => {
+    res.send(path.join(__dirname, 'index.html'))
+});
 
 //listens for any incoming connections on specified port (port defined above)
 app.listen(PORT, () => {
