cd9c691f9db4131a69b2d872c520dfdc3f23e6ea
Calvin Swomley <calvinswomley@gmail.com>
2021-11-12 13:20:09 -0700

    Post route setup

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -32,15 +32,24 @@ const getNotes = () =>
       'Content-Type': 'application/json',
     },
   });
+  console.log('step4 get the notes')
 
-const saveNote = (note) =>
+  const saveNote = (note) =>
   fetch('/api/notes', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
-  });
+  })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log('Successful POST request:', data);
+    return data;
+    })
+    .catch((error) => {
+      console.error('Error in POST request:', error);
+    });
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
@@ -72,8 +81,11 @@ const handleNoteSave = () => {
     text: noteText.value,
   };
   saveNote(newNote).then(() => {
+    console.log('step1')
     getAndRenderNotes();
+    console.log('step2')
     renderActiveNote();
+    console.log('step3')
   });
 };
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,37 +1,84 @@
+const { randomUUID } = require("crypto");
+const { json } = require("express");
 const express = require("express");
+const fs = require("fs");
 
 //Need absolute path so define it here. Note that 'path' comes with NodeJS
 const path = require("path");
-
 const db = require("./db/db.json");
 const PORT = 3003;
 const app = express();
-
-
 
 //allow express to parse data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //set up of static middleware >> This will grab all the static files in the 'public' folder
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+//function for adding to file
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+        console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            const stringifiedData = JSON.stringify(parsedData);
+            fs.writeFile(file, stringifiedData, 'utf8', (err) => {
+                if (err)
+                    console.log(err);
+                else {
+                    console.log("File written successfully\n");  
+                }
+            });
+        }
+    });
+};
 
 //define routes (underscore means 'req' is being taken in but not used)
-
-//1. post route
+//1. post route to add note
 app.post('/api/notes', (req, res)=> {
-    res.send("Hello World")
-})
+    console.info(`${req.method} request received to add note`);
+    
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: randomUUID(),
+        };
+        //const noteString = JSON.stringify(newNote);
+        const dbUrl = path.join(__dirname, './db/db.json');
+        readAndAppend(newNote, dbUrl);
+        const response = {
+            status: 'success',
+            body: dbUrl,
+        };
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error: POST unsuccessful');
+    }
+});
+//1.2 post to update existing note //SEE EXPRESS DAY 2: 16 body parsing.
+//app.post('/api/notes/:review_id', (req, res) => {
+ ///   if(req.body && req.params.note_id)
+//})
 
-//2. get route (/api/notes - read and return all notes from db.json)
-app.get('/api/notes', (__, res) => res.json(db));
+//2.[WORKS] get route (/api/notes - read and return all notes from db.json)
+app.get('/api/notes', (req, res) => {
+    console.log(`${req.method} recieved to get ALL notes`)
+    res.sendFile(path.join(__dirname, './db/db.json'));
+});
 
-//3. get route (/notes - return notes.html file)
+//2.2 get route (  - read and return specific note)
+
+//3. [WORKS] get route (/notes - return notes.html file)
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+});
 
-//4. get route (* - return index.html file)
+//4. [WORKS] get route (* - return index.html file)
 app.get('/', (req, res) => {
     res.send(path.join(__dirname, 'index.html'))
 });
@@ -40,4 +87,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     //do something
     console.log(`listening on port ${PORT}`)
-})
+});
